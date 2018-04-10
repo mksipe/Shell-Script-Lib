9acc6bd483a64b6709f6d4af7e01b83142e0f5c7
Mason Sipe <37341129+mksipe@users.noreply.github.com>
2018-04-10 18:31:09 -0500

    Add files via upload

--- INITIALIZE.sh
+++ INITIALIZE.sh
@@ -1,35 +1,34 @@
 #!/bin/sh
 
 echo "Initializing Directories"
-apt install nano -y
-apt install yum -y
-apt install vim -y
-apt install nmap -y
-apt install unzip -y
-apt install aptitude -y 
-apt install clamav -y
+apt-get install nano -y
+apt-get install yum -y
+apt-get install vim -y
+apt-get install nmap -y
+apt-get install unzip -y
+apt-get install aptitude -y 
+apt-get install clamav -y
 mkdir System\ Information\ Gathering
 chmod 755 Startup.sh
 mv Current_Connections.sh System\ Information\ Gathering
-mv Hacking-Tool-Search.sh System\ Information\ Gathering
 mv Network\ Status.sh System\ Information\ Gathering
 mv System\ Information.sh System\ Information\ Gathering
 mv Startup.sh System\ Information\ Gathering
 mkdir git
 mv *.md git
-mv LICENSE git 
+mv LICENSE git
 mkdir Firewall-Tools
 chmod 755 UFWC.sh
 mv UFWC.sh Firewall-Tools
-chmod 755 Malware-Detection
 mkdir Malware-Detection
+chmod 755 Malware-Detection
 chmod 755 clamAVconf.sh
 mv clamAVconf.sh Malware-Detection
 mkdir Network-Tools
 chmod 755 Netspoof-Prevention.sh
 mv Netspoof-Prevention.sh Network-Tools
 chmod 755 ARP-Spoof-Prevention
-mv ARP-Spoof-Prevention.sh Network-Tools
+mv ARP-Spoof-Prevention.sh Network-Tools > in
 mkdir Remote\ Logon
 chmod 755 Remote-Services-Settings.sh
 mv Remote-Services-Settings.sh Remote\ Logon
@@ -40,12 +39,11 @@ chmod 755 User-Auth.sh
 mv User-Auth.sh User\ Policies
 mv Exploit-Search.sh System\ Information\ Gathering
 mv Firefox.sh System-Tuning
-mv ARP-Spoof-Prevention.sh Firewall-Tools
 chmod 755 Network-Scan.sh
 mv Network-Scan.sh Network-Tools
 mkdir Updates
 chmod 755 Updates
-chmod 755 Updates.sh
+chmod 755 Update.sh
 mv Update.sh Updates
 echo "Creating Readme"
 touch README.txt
