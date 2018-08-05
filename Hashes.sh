c3e0790d90eb6e70fd39fb7ab3a17ec3589c9446
Mason Sipe <37341129+mksipe@users.noreply.github.com>
2018-08-05 12:25:29 -0500

    Add files via upload

---
+++ Hashes.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+if [ $(/usr/bin/id -u) -ne 0 ]; then
+    echo "You must be root."
+fi
+echo "Hashing Filesystem. This will take a while."
+find -type f -exec md5sum '{}' \; > /bin/lib/sh/MK3S/data/md5sum.db
