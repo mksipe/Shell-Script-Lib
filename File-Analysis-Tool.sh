ddae86ce32f1197320215ded8dbe291a231503c5
Mason Sipe <37341129+mksipe@users.noreply.github.com>
2018-09-15 23:43:00 -0500

    Add files via upload

--- File-Analysis-Tool.sh
+++ File-Analysis-Tool.sh
@@ -5,21 +5,22 @@ if [ $# -ne 1 ] ; then
 exit 255
 fi 
 touch "$1-Analysis"
-echo "####################" | tee -a $1-Analysis
+echo "===========================================" | tee -a $1-Analysis
 echo "$1 Basic Attributes" | tee -a $1-Analysis
-echo "####################" | tee -a $1-Analysis
-echo "Owner Info:" ; stat $1 | tee -a $1-Analysis ; echo "####################"
-echo "Type:" ; file $1 | tee -a $1-Analysis ; echo "####################"
-echo "S.O.D:" ; ldd $1 | tee -a $1-Analysis ; echo "####################"
-echo "L.C.T:" ; ltrace $1 | tee -a $1-Analysis ; echo "####################"
-echo "Hash (Predicted):" ; md5sum $1 | tee -a $1-Analysis ; echo "####################"
-echo "Hash (Pregiven):" ; md5sum -c $1 | tee -a $1-Analysis ; echo "####################"
-echo "Lines:" ; wc -l $1 | tee -a $1-Analysis ; echo "####################"
+echo "===========================================" | tee -a $1-Analysis
+echo "Owner Info:" ; stat $1 | tee -a $1-Analysis ; echo "==========================================="
+echo "Type:" ; file $1 | tee -a $1-Analysis ; echo "==========================================="
+echo "S.O.D:" ; ldd $1 | tee -a $1-Analysis ; echo "==========================================="
+echo "L.C.T:" ; ltrace $1 | tee -a $1-Analysis ; echo "==========================================="
+echo "Hash (Predicted):" ; md5sum $1 | tee -a $1-Analysis ; echo "==========================================="
+echo "Hash (Pregiven):" ; md5sum -c $1 | tee -a $1-Analysis ; echo "==========================================="
+echo "Lines:" ; wc -l $1 | tee -a $1-Analysis ; echo "==========================================="
 echo "Charachters:" ; wc -m $1 | tee -a $1-Analysis
-echo "####################" | tee -a $1-Analysis
+echo "===========================================" | tee -a $1-Analysis
 echo "Ransom/Malware Attributes" | tee -a $1-Analysis
-echo "####################" | tee -a $1-Analysis
+echo "===========================================" | tee -a $1-Analysis
 echo "ClamAV:" ; clamscan $1 | tee -a $1-Analysis
-echo "####################" | tee -a $1-Analysis
+echo "===========================================" | tee -a $1-Analysis
+echo "Team Cymru:" ; whois -h hash.cmyru.com $(md5sum $1)
 mkdir /bin/lib/sh/MK3S/data/Analysis > /dev/null
 mv $1-Analysis /bin/lib/sh/MK3S/data/Analysis/
