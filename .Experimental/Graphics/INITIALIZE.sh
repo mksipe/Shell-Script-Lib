852f089f8005d6e1fc2ac6e8114ee6ee6c2e557a
Mason Sipe <37341129+mksipe@users.noreply.github.com>
2018-11-16 15:47:42 -0600

    Add files via upload

---
+++ .Experimental/Graphics/INITIALIZE.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+if [ $(/usr/bin/id -u) -ne 0 ]; then
+    echo "You must be root."
+    exit
+fi
+if [ ! -e /bin/lib/sh/MK3S/data/badips.db ] ; then
+ echo MK3SL is not installed.
+ exit 1                      
+fi
+mkdir /Framework
+mv *.html /Framework
+mv *.css /Framework
+mv *.git /Framework
+mv *.github /Framework
+mkdir /bin/lib/public
+mkdir /bin/lib/public/data
+mkdir /home/$USER/Desktop/SSL
+mkdir /bin/lib/sh/MK3S/data/web
+mv *.html /bin/lib/sh/MK3S/data/web
+mv *.css /bin/lib/sh/MK3S/data/web
+ln /bin/lib/public/data/web/HTSA.html /home/$USER/Desktop/SSL/HTSA
+rm -r /Framework
