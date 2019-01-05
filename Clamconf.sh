fe6a4b6aeb04aa6ba899946e6b1fa61a0815b676
Mason Sipe <37341129+mksipe@users.noreply.github.com>
2019-01-05 15:16:13 -0600

    Update Clamconf.sh

--- Clamconf.sh
+++ Clamconf.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apt install clamav -y 
 sudo freshclam
-echo "0 12 * * * bash clamscan -rd --remove /" | sudo tee -a /etc/crontab
+echo "0 12 * * * bash clamscan / -rd --remove /" | sudo tee -a /etc/crontab
 echo [SUCCESS] banners.sh was ran by $USER on $(date -u) | tee -a /bin/lib/sh/MK3S/data/MK3S.log
-clamscan -rd --remove /
+clamscan / -rd --remove /
